c8184d48ff7bfac579d0a61714424cd84087b3fb
Arun George Zachariah <azachariah@mail.missouri.edu>
2020-10-18 09:27:28 -0500

    Minor update.

--- Reproduce_Results/setup_scripts/setup_crow.sh
+++ Reproduce_Results/setup_scripts/setup_crow.sh
@@ -5,7 +5,7 @@ HOME=${HOME}
 QIK_HOME=${PWD}/../..
 
 # Obtaining the model.
-# wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel -O $QIK_HOME/ML_Models/CroW/vgg/VGG_ILSVRC_16_layers.caffemodel
+wget http://www.robots.ox.ac.uk/~vgg/software/very_deep/caffe/VGG_ILSVRC_16_layers.caffemodel -O $QIK_HOME/ML_Models/CroW/vgg/VGG_ILSVRC_16_layers.caffemodel
 
 # Activating the conda environment.
 source activate deepvision
